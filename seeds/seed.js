2a49984c4165f4b70d2ab03b35e8b2099fef15bc
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-21 16:35:14 -0700

    seeds draft needs major work

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,36 @@
-const sequelize = require('../config/connection');
-const { User, Posts, Comments } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Posts, Comments } = require("../models");
 
-const userData = require('./userData.json');
-const postsData = require('./postsData.json');
-const commentsData = require('./commentsData.json');
+const userData = require("./userData.json");
+const postsData = require("./postsData.json");
+const commentsData = require("./commentsData.json");
 
 const seedDatabase = async () => {
-//  develop this later
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const posts of postsData) {
+    await Posts.create({
+      ...posts,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+
+    for (const comments of commentsData) {
+      await Comments.create({
+        ...comments,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        post_id: posts[Math.floor(Math.random() * posts.length)].id,
+      });
+    }
+  }
+
+  process.exit(0);
 };
 
 seedDatabase();
+
+// Come back to this later. I think this is not set up correctly.
