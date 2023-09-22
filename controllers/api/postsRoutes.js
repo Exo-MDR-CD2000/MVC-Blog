df6d31646847c52ec92c3a109cd4cebf80313e72
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-22 12:06:01 -0700

    added post, put and delete routes

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -33,3 +33,63 @@ router.get("/:id", withAuth, async (req, res) => {
 });
 
 // POST create a new post
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Posts.create({
+      ...req.body,
+      // if spread operator doesn't work, try this:
+      // title: req.body.title,
+      // content: req.body.content,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// PUT update a post by id
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Posts.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+    if (!postData) {
+      res.status(404).json({ message: "No post can be found with that id!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a post by id
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Posts.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: "No post can be found with that id!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
