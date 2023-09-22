89ad53f13e35298f0c09702fc4c4a7392fdb581d
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-21 17:30:38 -0700

    rearranged seed file and added routes files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,36 @@
+const sequelize = require('../config/connection');
+const { User, Posts, Comments } = require('../models');
+
+const userData = require('./userData.json');
+const postsData = require('./postsData.json');
+const commentsData = require('./commentsData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    // await User.bulkCreate(userData);
+
+    await Posts.bulkCreate(postsData);
+    await Comments.bulkCreate(commentsData);
+
+    console.log(users); // log the created users to the console
+    console.log(postsData); // log the created posts to the console
+    console.log(commentsData); // log the created comments to the console
+
+    process.exit(0);
+};
+
+seedDatabase();
+
+// mysql shell looks fine when viewing the tables. passwords are hashed and not visible. Maybe I don't need top use for loops or math.floor
+
+
+
 // const sequelize = require("../config/connection");
 // const { User, Posts, Comments } = require("../models");
 
@@ -35,37 +68,3 @@
 
 // Come back to this later. I think this is not set up correctly.
 
-
-
-
-
-const sequelize = require('../config/connection');
-const { User, Posts, Comments } = require('../models');
-
-const userData = require('./userData.json');
-const postsData = require('./postsData.json');
-const commentsData = require('./commentsData.json');
-
-const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
-
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    // await User.bulkCreate(userData);
-
-    await Posts.bulkCreate(postsData);
-    await Comments.bulkCreate(commentsData);
-
-    console.log(users); // log the created users to the console
-    console.log(postsData); // log the created posts to the console
-    console.log(commentsData); // log the created comments to the console
-
-    process.exit(0);
-};
-
-seedDatabase();
-
-// mysql shell looks fine when viewing the tables. passwords are hashed and not visible. Maybe I don't need top use for loops or math.floor
