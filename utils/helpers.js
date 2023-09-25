0cd2c9fbae0b19e414c512b45fafca2ae7849ffc
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-25 15:04:32 -0700

    updated time helper and homepage handlebar

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,24 @@
 module.exports = {
-  formatDate: function(date) {
+  formatDate: function(dateString) {
+    const date = new Date(dateString);
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-    return new Date(date).toLocaleDateString('en-US', options);
+    return date.toLocaleDateString('en-US', options);
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = {
+//   formatDate: function(date) {
+//     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+//     return new Date(date).toLocaleDateString('en-US', options);
+//   }
+// };
