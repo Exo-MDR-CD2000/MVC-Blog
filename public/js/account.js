80d8683f38219f63a626f64fce03bdd23f4d47f8
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-09-25 17:20:57 -0700

    delete user post good. left testing editing code

--- public/js/account.js
+++ public/js/account.js
@@ -1,5 +1,5 @@
 // develop user account functionality
-// needs to allow post creation with title and content fields working
+// Does the route need to be changed to show the user's posts?
 // needs to allow post deletion
 // needs to allow post editing
 
@@ -25,3 +25,72 @@ newPostForm.addEventListener("submit", async (event) => {
     alert("Fields cannot be empty");
   }
 });
+
+// Edit post form handler
+// const editPostFormHandler = async (event) => {
+//     event.preventDefault();
+//     const title = document.querySelector("#edit-post-title").value.trim();
+//     const content = document.querySelector("#edit-post-content").value.trim();
+//     const id = document.querySelector("#edit-post-id").value.trim();
+  
+//     if (title && content && id) {
+//       const response = await fetch(`/api/posts/${id}`, {
+//         method: "PUT",
+//         body: JSON.stringify({ title, content }),
+//         headers: { "Content-Type": "application/json" },
+//       });
+  
+//       if (response.ok) {
+//         window.location.reload();
+//       } else {
+//         alert("Failed to update post");
+//       }
+//     } else {
+//       alert("Fields cannot be empty");
+//     }
+//   };
+    
+  // Delete post handler
+  const deletePostHandler = async (event) => {
+      event.preventDefault();
+      const id = event.target.getAttribute("data-post-id");
+    
+      if (id) {
+        const response = await fetch(`/api/posts/${id}`, {
+          method: "DELETE",
+        });
+    
+        if (response.ok) {
+          window.location.reload();
+        } else {
+          alert("Failed to delete post");
+        }
+      }
+    };
+    
+//   // Add event listeners to edit and delete post buttons
+//   const editPostButtons = document.querySelectorAll(".edit-post");
+//   editPostButtons.forEach((button) => {
+//     button.addEventListener("click", async (event) => {
+//       const id = event.target.getAttribute("data-post-id");
+//       const response = await fetch(`/api/posts/${id}`);
+//       const post = await response.json();
+  
+//       // Populate edit post form with post data
+//       document.querySelector("#edit-post-title").value = post.title;
+//       document.querySelector("#edit-post-content").value = post.content;
+//       document.querySelector("#edit-post-id").value = post.id;
+  
+//       // Show edit post form
+//       document.querySelector("#edit-post-form").classList.remove("d-none");
+//     });
+//   });
+  
+  const deletePostButtons = document.querySelectorAll(".delete-post");
+  deletePostButtons.forEach((button) => {
+    button.addEventListener("click", deletePostHandler);
+  });
+  
+//   // Add event listener to edit post form
+//   const editPostForm = document.querySelector("#edit-post-form");
+//   editPostForm.addEventListener("submit", editPostFormHandler);
